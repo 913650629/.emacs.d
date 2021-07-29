201aa903f5aac2ca75214320ffa78aba13fbf329
lzh <913650629@qq.com>
2021-07-29 14:25:29 +0800

    dupm文件添加只读权限

--- script/dump.sh
+++ script/dump.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-LC_TYPE=en_US.UTF-8 emacs --batch -q -l $HOME/.emacs.d/dump.el
+chmod 644 $HOME/.emacs.d/emacs.pdmp && LC_TYPE=en_US.UTF-8 emacs --batch -q -l $HOME/.emacs.d/dump.el && chmod 444 $HOME/.emacs.d/emacs.pdmp
