6f71a0871bbf5fc2f51f161ac7ea3d58a1141470
lzh <913650629@qq.com>
2021-08-17 14:30:20 +0800

    细节细节

--- etc/config/init-org.el
+++ etc/config/init-org.el
@@ -70,7 +70,7 @@
    ;; 代码捕获模板
      (push '("c" "计划" entry (file+headline "~/Documents/org/day.org" "待办") "* [要事] %^{要事} %?\n  创建时间: %U\n  开始时间: %^t\n  截至时间: %^t") org-capture-templates)
      (push '("t" "待办" entry (file+headline "~/Documents/org/day.org" "待办") "* [要事] %^{要事}%?\n  创建时间: %U") org-capture-templates)
-     (push '("d" "今日" entry (file+datetree "~/Documents/org/day.org") "* 今日时间\n** [今日待办] %?\n** [上午] 8:30-11:30\n   上午建议产出时间：150min\n\t- [ ] 规划15x\n\t- [ ] 学习\n\t  * [ ] 视频40x\n\t  * [ ] 学习30x\n\t- [ ] 工作30x\n** [下午] 13:00-18:00\n   下午建议产出时间：240min\n\t- [ ] 规划15x\n\t- [ ] 学习\n\t  * [ ] 视频40x\n\t  * [ ] 学习30x\n\t- [ ] 工作30x\n** [晚间] 19:00-23:30\n   晚上建议产出时间：120min\n\t- [ ] 学习\n\t  * [ ] 视频40x\n\t  * [ ] 学习30x") org-capture-templates))
+     (push '("d" "今日" entry (file+datetree "~/Documents/org/day.org") "* 今日时间\n** [今日待办] %?\n** [上午] 8:30-11:30\n   建议产出时间：150min\n\t- [ ] 规划15x\n\t- [ ] 学习\n\t  * [ ] 视频40x\n\t  * [ ] 学习30x\n\t- [ ] 工作30x\n** [下午] 13:00-18:00\n   建议产出时间：240min\n\t- [ ] 规划15x\n\t- [ ] 学习\n\t  * [ ] 视频40x\n\t  * [ ] 学习30x\n\t- [ ] 工作30x\n** [晚间] 19:00-23:30\n   建议产出时间：120min\n\t- [ ] 学习\n\t  * [ ] 视频40x\n\t  * [ ] 学习30x") org-capture-templates))
 
 (use-package org-agenda
   :after org
